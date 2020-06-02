d63c1c1b3a149928b05b1c192f75a12aac671e7b
Stefano <stefano.huber@gmail.com>
2020-06-02 13:05:22 +0200

    Add images_cls

--- tasks/images_cls.py
+++ tasks/images_cls.py
@@ -4,37 +4,62 @@ Images classification
 
 import torch
 import torch.nn as nn
+from torchvision import datasets, transforms
+import torch.nn.functional as F
 
 
-class SimpleLSTM(nn.Module):
-    def __init__(
-        self, embedding_dim=300, hidden_dim=100, vocab_size=3000, output_size=10
-    ):
-        super(SimpleLSTM, self).__init__()
-        self.hidden_dim = hidden_dim
+class Cnn(nn.Module):
+    # The network is a CNN, with one convolutional layer, dropout, and 2 fully connected layers
+    def __init__(self, dropout=0.5):
+        super(Cnn, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 5, 1)   # Kernel size of 5, TODO: it may be a good parameter to optimize with
+        self.dropout1 = nn.Dropout2d(dropout)
+        # The size is computed as 32 (filters) * 24 (height after applying kernel) * 24 (width after applying kernel)
+        self.fc1 = nn.Linear(18432, 500)
+        self.dropout2 = nn.Dropout2d(dropout)
+        self.fc2 = nn.Linear(500, 10)
 
-        self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
-
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim)
-
-        self.out = nn.Linear(hidden_dim, output_size)
-
-    def forward(self, sentence):
-        embedding = self.word_embeddings(sentence)
-
-        lstm_out, _ = self.lstm(embeds.view(len(sentence), 1, -1))
-
-        return self.out(lstm_out.view(len(sentence), -1))
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        # x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
 
 
 def get_model():
-    return SimpleLSTM()
-
-
-"""
-Load data
-"""
+    """
+    Returns the model, in this case a CNN
+    Returns:
+        model: of type nn.Module
+    """
+    return Cnn
 
 
 def get_data():
-    return []
+    """
+    Return a DataLoader for the training data.
+    Returns:
+        dataset: of type DataLoader
+    """
+    # Initialize the training loader, use special parameters if cuda is available.
+    use_cuda = torch.cuda.is_available()
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('data',
+                       train=True,
+                       download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))  # Parameters advised by Torch documentation
+                       ])),
+        batch_size=64,
+        shuffle=True,
+        **kwargs)
+    return train_loader
