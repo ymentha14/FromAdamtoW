1c7513a408b8b21b424a2787dfe66d2998d71afe
Stefano <stefano.huber@gmail.com>
2020-06-04 01:22:13 +0200

    Avoid to save the model, still need to add proper checkpoint for that at the end of cross validation

--- src/pytorchtools.py
+++ src/pytorchtools.py
@@ -27,10 +27,10 @@ class EarlyStopping:
     def __call__(self, val_loss, model):
 
         score = -val_loss
-
+        
         if self.best_score is None:
             self.best_score = score
-            self.save_checkpoint(val_loss, model)
+            # self.save_checkpoint(val_loss, model)
         elif score < self.best_score + self.delta:
             self.counter += 1
             print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
@@ -38,7 +38,7 @@ class EarlyStopping:
                 self.early_stop = True
         else:
             self.best_score = score
-            self.save_checkpoint(val_loss, model)
+            # self.save_checkpoint(val_loss, model)
             self.counter = 0
 
     def save_checkpoint(self, val_loss, model):
